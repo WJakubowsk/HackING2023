62672f647334623d2f553daeb643d91bbcdc6dc9
WJakubowsk <72559388+WJakubowsk@users.noreply.github.com>
2023-05-18 14:56:53 +0200

    feat: add lang detection & spellcheck

--- src/preprocessing/preprocessor.py
+++ src/preprocessing/preprocessor.py
@@ -1,24 +1,45 @@
 import numpy as np
 import pandas as pd
 from autocorrect import Speller
+from langdetect import detect
 class Preprocessor:
     def __init__(self, df: pd.DataFrame) -> None:
         self.df = df
     
     def preprocess_text(self, text_col: str):
-        self._convert_text_to_lowercase(text_col)
+        self.df[text_col] = self.df[text_col].apply(lambda x: str(x))
+        self.convert_text_to_lowercase(text_col)
+        self.detect_language(text_col)
+        self.autocorrect_words(text_col)
         pass
 
     def preprocess_image(self, image_col):
         #TODO
         pass
     
-    def _convert_text_to_lowercase(self, text_col: str):
+
+    def detect_language(self, text_col: str):
+        def _detect_with_ignore(text: str):
+            try:
+                lang = detect(text)
+            except:
+                lang='en'
+            return lang
+        self.df['language'] = self.df[text_col].apply(lambda x: _detect_with_ignore(x))
+
+    def convert_text_to_lowercase(self, text_col: str):
         self.df[text_col] = self.df[text_col].str.lower()
     
-    def _autocorrect_words(self, text_col: str, language: str = 'en'):
-        spell = Speller(language=language, only_replacements=True) # for OCR detected text it should be better
-        self.df[text_col] = self.df[text_col].apply(lambda x: spell(str(x)))
+    def autocorrect_words(self, text_col: str):
+        languages = set(self.df['language'])
+        for lang in languages:
+            try:
+                spell = Speller(lang = lang, only_replacements=True)
+            except:
+                spell = Speller(lang = 'en', only_replacements=True)
+            self.df[text_col] = np.where(self.df['language'] == lang, self.df[text_col].apply(lambda x: spell(x)), self.df[text_col])
+
+        
     
 
 
