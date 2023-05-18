800cb0ce57afe782b9be7d0e4a7bcf1fb3407b3e
WJakubowsk <72559388+WJakubowsk@users.noreply.github.com>
2023-05-18 13:39:55 +0200

    feat: add spellcheck

--- src/preprocessing/preprocessor.py
+++ src/preprocessing/preprocessor.py
@@ -1,8 +1,11 @@
+import numpy as np
+import pandas as pd
+from autocorrect import Speller
 class Preprocessor:
-    def __init__(self, df) -> None:
+    def __init__(self, df: pd.DataFrame) -> None:
         self.df = df
     
-    def preprocess_text(self, text_col):
+    def preprocess_text(self, text_col: str):
         self._convert_text_to_lowercase(text_col)
         pass
 
@@ -10,6 +13,12 @@ class Preprocessor:
         #TODO
         pass
     
-    def _convert_text_to_lowercase(self, text_col):
+    def _convert_text_to_lowercase(self, text_col: str):
         self.df[text_col] = self.df[text_col].str.lower()
+    
+    def _autocorrect_words(self, text_col: str, language: str = 'en'):
+        spell = Speller(language=language, only_replacements=True) # for OCR detected text it should be better
+        self.df[text_col] = self.df[text_col].apply(lambda x: spell(str(x)))
+    
+
 
