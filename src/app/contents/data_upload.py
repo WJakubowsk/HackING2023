d7a06fa7929e7ddf7b6d7f924221b527a4f3bc2a
WJakubowsk <72559388+WJakubowsk@users.noreply.github.com>
2023-05-19 10:15:09 +0200

    add model to demo

--- src/app/contents/data_upload.py
+++ src/app/contents/data_upload.py
@@ -2,6 +2,12 @@ import streamlit as st
 import numpy as np
 import easyocr
 from PIL import Image
+import pandas as pd
+import pickle
+import torch
+from src.preprocessing.preprocessor import Preprocessor
+from src.modeling.model import Model
+
 
 def parse_image(file):
     img = Image.open(file)
@@ -15,9 +21,23 @@ def apply_ocr(parsed_file):
     print(text)
     return text
 
+def preprocess_text(text: str) -> pd.DataFrame:
+    df = pd.DataFrame({'Text': [text]})
+    preprocessor = Preprocessor(df)
+    preprocessor.preprocess_text('Text', lemmatize=True)
+    df_preprocessed = preprocessor.df
+    return df_preprocessed
+
+
+def get_inference(obs: pd.Series, model: Model):
+    model.predict_obs(0.6)
 
 def run():
     st.markdown("## Data Upload")
+
+    tfidf_vectorizer = pickle.load('pretrained/tfidf_vectorizer.pkl')
+    text_model = pickle.load('pretrained/svm_model.pkl')
+    vision_model = torch.load('pretrained/cnn_model.pth')    
 
     uploaded_image = st.file_uploader("Choose an image", type=["tiff", "jpg"])
 
@@ -26,9 +46,13 @@ def run():
         st.image(uploaded_image, width=700)
 
         st.markdown("Detected text:")
-        st.write(str(apply_ocr((parse_image(uploaded_image)))))
+        ocr_string = apply_ocr((parse_image(uploaded_image)))
+        st.write(ocr_string)
+
+        st.markdown("Preprocessed text:")
+        preprocessed_df = preprocess_text(ocr_string)
 
         st.markdown("Predictions:")
+        model = Model(vision_model, text_model, tfidf_vectorizer, preprocessed_df, uploaded_image)
+        st.write(model.predict_obs(0.6))
 
-        
-    return parse_image(uploaded_image)
--- src/app/contents/interference.py
+++ src/app/contents/interference.py
@@ -1,6 +1,7 @@
 import pandas as pd
-
+import pickle
 from src.preprocessing.preprocessor import Preprocessor
+from src.modeling.model import Model
 
 
 def preprocess_text(text):
@@ -9,3 +10,7 @@ def preprocess_text(text):
     preprocessor.preprocess_text('Text', lemmatize=True)
     df_preprocessed = preprocessor.df
     return df_preprocessed
+
+
+def get_inference(obs: pd.Series):
+    model = Model()
