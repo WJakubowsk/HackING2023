eca6f77262d1fba4cdf6206c1df5eb263d7cda16
WJakubowsk <72559388+WJakubowsk@users.noreply.github.com>
2023-05-19 08:26:08 +0200

    feat: add more feature engineerring

--- src/feature_engineering/feature_extractor.py
+++ src/feature_engineering/feature_extractor.py
@@ -6,7 +6,15 @@ from typing import List
 class FeatureExtractor:
     def __init__(self, df):
         self.df = df
-    
+
+    def feature_engineering(self, text_col: str, terms: List[str], fuzzy_match_treshold: float):
+        self.get_word_count(text_col)
+        self.get_digits_count(text_col)
+
+        for term in terms:
+            print('extracting feature for term ', term)
+            self.get_binary_str_col(text_col=text_col, term=term, fuzzy_match_threshold=fuzzy_match_treshold)
+
     @staticmethod
     def _fuzzy_match_string(string_list: List[str], term: str):
         return max([fuzz.ratio(string, term) for string in string_list])
@@ -17,9 +25,10 @@ class FeatureExtractor:
         imperfect matches. Creates new binary column in the dataset, which represents the presence of the desired term
         """
         self.df[term] = self.df[text_col].apply(lambda x: 1 if (term in x) or (self._fuzzy_match_string(x, term) > fuzzy_match_threshold) else 0)
+
     
-    def feature_engineering(self, text_col: str, terms: List[str], fuzzy_match_treshold: float):
-        for term in terms:
-            print('extracting feature for term ', term)
-            self.get_binary_str_col(text_col=text_col, term=term, fuzzy_match_threshold=fuzzy_match_treshold)
-    
+    def get_word_count(self, text_col: str):
+        self.df['word_count'] = self.df[text_col].apply(lambda x: len(x))
+
+    def get_digits_count(self, text_col: str):
+        self.df['digits_count'] = self.df[text_col].apply(lambda x: sum([char.isdigit() for string in x for char in string]))
